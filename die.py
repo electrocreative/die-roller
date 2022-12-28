45aba2f83e1399ef7f63c742b10a5923d4d20ab4
Scott Henz <electrocreative@users.noreply.github.com>
2022-12-28 17:05:18 -0600

    Create die.py
    Initial ccommit

---
+++ die.py
@@ -0,0 +1,15 @@
+"""
+A simple die roller
+
+Author: Scott R. Henz
+Date: 09/02/2022
+"""
+
+import random
+
+first = int(input('Type the first number: '))
+last = int(input('Type the last number: '))
+roll = random.randint(first, last)
+
+print('Choosing a number between ' + str(first) + ' and ' + str(last) + '.')
+print('The number is ' + str(roll) + '.')
