8737382f2e45fb1423126fbff174766d7e22ea70
Scott Henz <electrocreative@users.noreply.github.com>
2022-12-28 17:22:02 -0600

    Update die.py

--- die.py
+++ die.py
@@ -11,5 +11,5 @@ first = int(input('Type the first number: '))
 last = int(input('Type the last number: '))
 roll = random.randint(first, last)
 
-print('Choosing a number between ' + str(first) + ' and ' + str(last) + '.')
+print('Choosing a number between ' + str(first) + ' and ' + str(last) + '...')
 print('The number is ' + str(roll) + '.')
